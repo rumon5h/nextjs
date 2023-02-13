65647170ceef5990392d5d1e8f027ee938312788
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-13 16:49:03 +0600

    catch all routes

---
+++ pages/docs/[[...params]].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router"
+
+function Docs(){
+    const router = useRouter();
+    const {params = []} = router.query;
+    console.log(params);
+    if(params.length === 2){
+        return <h1>Viewing docs for the feature {params[0]} and concept {params[1]}</h1>
+    }
+    else if(params.length == 1){
+        return <h1>Viewing docs for the feature {params[params[0]]}</h1>
+    }
+    return <h1>This is docs home page.</h1>
+}
+
+export default Docs
