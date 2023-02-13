2ab2222d5a3d7840444ed7e9b1ebd9aad0848a7b
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-13 17:21:54 +0600

    Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 function Home(){
-    return <h1>This is home page.</h1>
+    return <div>
+        <h1><Link href='/product'>Product page</Link></h1>
+        <h1>This is home page.</h1>
+        <Link href='/blog'>
+        Blog
+        </Link>
+    </div>
 }
 
 export default Home;
